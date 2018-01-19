99aeb4c7ccc2a814ace5cd848c7d62576c721146
Ksusha Zhukova <zhukovaksusha@gmail.com>
2018-01-19 11:42:10 +0300

    update var names

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -5,12 +5,11 @@ from pytz import timezone
 import pytz
 
 
-def load_attempts():
-    url = "https://devman.org/api/challenges/solution_attempts/?page=2"
-    info = requests.get(url).json()
+def load_attempts(url):
+    data_from_devman_api = requests.get(url).json()
     pages = 1
     for page in range(pages):
-        for user_info in info["records"]:
+        for user_info in data_from_devman_api["records"]:
             yield {
                 'username': user_info['username'],
                 'timestamp': user_info['timestamp'],
@@ -18,19 +17,24 @@ def load_attempts():
             }
 
 
-def get_midnighters(dict_of_users):
+def get_midnighters(dict_of_user):
+    list_of_midnighters = []
     utc = pytz.utc
-    for user_info in dict_of_users:
+    for user_info in dict_of_user:
         user_time_zone = timezone(user_info["timezone"])
         time_stamp = user_info["timestamp"]
         fmt = '%H:%M'
-        local_time = utc.localize(datetime.utcfromtimestamp(time_stamp))
-        time_in_user_time_zone = local_time.astimezone(user_time_zone)
+        user_local_time = utc.localize(datetime.utcfromtimestamp(time_stamp))
+        time_in_user_time_zone = user_local_time.astimezone(user_time_zone)
         if time_in_user_time_zone.strftime(fmt) > "00:00":
             if time_in_user_time_zone.strftime(fmt) < "05:00":
-                print(user_info["username"])
+                list_of_midnighters.append(user_info["username"])
+    return list_of_midnighters
 
 if __name__ == '__main__':
+    url = "https://devman.org/api/challenges/solution_attempts/?page=2"
+    active_devman_users = load_attempts(url)
+    list_of_midnighters = get_midnighters(active_devman_users)
     print("The midnighters are: ")
-    midnighters = load_attempts()
-    get_midnighters(midnighters)
+    for user_name in list_of_midnighters:
+        print(user_name)
