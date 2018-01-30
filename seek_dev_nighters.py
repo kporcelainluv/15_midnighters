53ed56f2f25ea9d04f3ef0dd2b328083b9b58430
Ksusha Zhukova <zhukovaksusha@gmail.com>
2018-01-30 17:30:23 +0300

    update var names, timestamp usage

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -14,23 +14,24 @@ def load_attempts(url, payload):
         }
 
 
-def get_midnighters(dict_of_user_info):
-    list_of_midnighters = set()
-    for user_info in dict_of_user_info:
+def get_midnighters(dict_of_users_info):
+    midnighters = set()
+    for user_info in dict_of_users_info:
         user_time_zone = timezone(user_info["timezone"])
         time_stamp = user_info["timestamp"]
         fmt = '%H'
-        user_commit_time = user_time_zone.localize(datetime.fromtimestamp(time_stamp))
-        if user_commit_time.strftime(fmt) >= "00" and user_commit_time.strftime(fmt) <= "05":
-            list_of_midnighters.add(user_info["username"])
-    return list_of_midnighters
+        user_commit_time = datetime.fromtimestamp(time_stamp, tz=user_time_zone)
+        if "00" <= user_commit_time.strftime(fmt) <= "05":
+            midnighters.add(user_info["username"])
+    return midnighters
 
 
 if __name__ == '__main__':
     url = "https://devman.org/api/challenges/solution_attempts/"
-    payload = {'page': 2}
-    active_devman_users = load_attempts(url, payload)
-    midnighters = get_midnighters(active_devman_users)
     print("The midnighters are: ")
-    for user_name in midnighters:
-        print(user_name)
+    for page_number in range(1, 11):
+        payload = {'page': page_number}
+        active_devman_users = load_attempts(url, payload)
+        midnighters = get_midnighters(active_devman_users)
+        for user_name in midnighters:
+            print(user_name)
