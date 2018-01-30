2ecbea938c7055817fbfd08ae2ee8b3aa7c53af9
Ksusha Zhukova <zhukovaksusha@gmail.com>
2018-01-30 23:47:56 +0300

    simplified funcs, updated var names

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -4,9 +4,12 @@ from datetime import datetime
 from pytz import timezone
 
 
-def load_attempts(url, payload):
-    data_from_devman_api = requests.get(url, params=payload).json()
-    for user_info in data_from_devman_api["records"]:
+def extract_data_from_url(payload):
+    return requests.get(url, params=payload).json()
+
+
+def load_user_info(data_attempt):
+    for user_info in data_attempt["records"]:
         yield {
             'username': user_info['username'],
             'timestamp': user_info['timestamp'],
@@ -14,14 +17,16 @@ def load_attempts(url, payload):
         }
 
 
-def get_midnighters(dict_of_users_info):
+def get_midnighters(users_info):
     midnighters = set()
-    for user_info in dict_of_users_info:
+    for user_info in users_info:
         user_time_zone = timezone(user_info["timezone"])
         time_stamp = user_info["timestamp"]
-        fmt = '%H'
-        user_commit_time = datetime.fromtimestamp(time_stamp, tz=user_time_zone)
-        if "00" <= user_commit_time.strftime(fmt) <= "05":
+        user_commit_time = (datetime.fromtimestamp(time_stamp, tz=user_time_zone)).hour
+
+        beginning_of_midnight = 0
+        end_of_midnight = 5
+        if beginning_of_midnight <= user_commit_time <= end_of_midnight:
             midnighters.add(user_info["username"])
     return midnighters
 
@@ -29,9 +34,18 @@ def get_midnighters(dict_of_users_info):
 if __name__ == '__main__':
     url = "https://devman.org/api/challenges/solution_attempts/"
     print("The midnighters are: ")
-    for page_number in range(1, 11):
-        payload = {'page': page_number}
-        active_devman_users = load_attempts(url, payload)
+    set_of_users = set()
+    page = 1
+    number_of_pages = extract_data_from_url(payload={'page': page})["number_of_pages"]
+
+    while page <= number_of_pages:
+        payload = {'page': page}
+        data_attempt = extract_data_from_url(payload)
+        active_devman_users = load_user_info(data_attempt)
         midnighters = get_midnighters(active_devman_users)
-        for user_name in midnighters:
-            print(user_name)
+        for user in midnighters:
+            set_of_users.add(user)
+        page += 1
+
+    for user in set_of_users:
+        print(user)
