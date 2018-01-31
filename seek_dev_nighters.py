beb97a7bb13abf3de8eb90ff3cc82670c11bb4f2
Ksusha Zhukova <zhukovaksusha@gmail.com>
2018-01-31 18:38:57 +0300

    update var names, remove redudundant code

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from pytz import timezone
 
 
-def extract_data_from_url(payload):
+def fetch_data_from_url(payload):
     return requests.get(url, params=payload).json()
 
 
@@ -19,33 +19,33 @@ def load_user_info(data_attempt):
 
 def get_midnighters(users_info):
     midnighters = set()
-    for user_info in users_info:
-        user_time_zone = timezone(user_info["timezone"])
-        time_stamp = user_info["timestamp"]
+    beginning_of_midnight = 0
+    end_of_midnight = 5
+    for attempt in users_info:
+        user_time_zone = timezone(attempt["timezone"])
+        time_stamp = attempt["timestamp"]
         user_commit_time = (datetime.fromtimestamp(time_stamp, tz=user_time_zone)).hour
-
-        beginning_of_midnight = 0
-        end_of_midnight = 5
         if beginning_of_midnight <= user_commit_time <= end_of_midnight:
-            midnighters.add(user_info["username"])
+            midnighters.add(attempt["username"])
     return midnighters
 
 
 if __name__ == '__main__':
     url = "https://devman.org/api/challenges/solution_attempts/"
     print("The midnighters are: ")
-    set_of_users = set()
+    set_of_midnighters = set()
     page = 1
-    number_of_pages = extract_data_from_url(payload={'page': page})["number_of_pages"]
+    number_of_pages = 1
 
     while page <= number_of_pages:
         payload = {'page': page}
-        data_attempt = extract_data_from_url(payload)
+        data_attempt = fetch_data_from_url(payload)
+        number_of_pages = data_attempt["number_of_pages"]
         active_devman_users = load_user_info(data_attempt)
         midnighters = get_midnighters(active_devman_users)
         for user in midnighters:
-            set_of_users.add(user)
+            set_of_midnighters.add(user)
         page += 1
 
-    for user in set_of_users:
+    for user in set_of_midnighters:
         print(user)
