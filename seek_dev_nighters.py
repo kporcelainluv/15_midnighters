e374dc021f00ba801083c019ca5cd1fd8a738a65
Ksusha Zhukova <zhukovaksusha@gmail.com>
2018-02-01 18:10:28 +0300

    update var names, removed redundunt code, used while cycle

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -9,19 +9,19 @@ def fetch_data_from_url(payload):
 
 
 def load_user_info(data_attempt):
-    for user_info in data_attempt["records"]:
+    for attempt in data_attempt["records"]:
         yield {
-            'username': user_info['username'],
-            'timestamp': user_info['timestamp'],
-            'timezone': user_info['timezone'],
+            'username': attempt['username'],
+            'timestamp': attempt['timestamp'],
+            'timezone': attempt['timezone'],
         }
 
 
-def get_midnighters(users_info):
+def get_midnighters(attempts):
     midnighters = set()
     beginning_of_midnight = 0
     end_of_midnight = 5
-    for attempt in users_info:
+    for attempt in attempts:
         user_time_zone = timezone(attempt["timezone"])
         time_stamp = attempt["timestamp"]
         user_commit_time = (datetime.fromtimestamp(time_stamp, tz=user_time_zone)).hour
@@ -35,17 +35,18 @@ if __name__ == '__main__':
     print("The midnighters are: ")
     set_of_midnighters = set()
     page = 1
-    number_of_pages = 1
 
-    while page <= number_of_pages:
+    while True:
         payload = {'page': page}
         data_attempt = fetch_data_from_url(payload)
-        number_of_pages = data_attempt["number_of_pages"]
-        active_devman_users = load_user_info(data_attempt)
-        midnighters = get_midnighters(active_devman_users)
-        for user in midnighters:
-            set_of_midnighters.add(user)
-        page += 1
+        if page < data_attempt["number_of_pages"]:
+            active_devman_users = load_user_info(data_attempt)
+            midnighters = get_midnighters(active_devman_users)
+            for user in midnighters:
+                set_of_midnighters.add(user)
+            page += 1
+        else:
+            break
 
     for user in set_of_midnighters:
         print(user)
